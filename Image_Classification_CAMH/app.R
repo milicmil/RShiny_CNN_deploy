d5b47e2c31cfc8292c3e66a4b0c72c297af0c308
milicmil <milos.milic@alum.utoronto.ca>
2022-07-26 00:12:39 -0400

    Shiny mnist example set up

--- Image_Classification_CAMH/app.R
+++ Image_Classification_CAMH/app.R
@@ -6,46 +6,57 @@
 #
 #    http://shiny.rstudio.com/
 #
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+library(keras)
+
+setwd("/Users/new/Documents/github_projects/RShiny_CNN_deploy/Image_Classification_CAMH")
+# Load the model
+model <- load_model_tf("cnn-mnist/")
+
+
+# Define the UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  # App title ----
+  titlePanel("Hello TensorFlow!"),
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      # Input: File upload
+      fileInput("image_path", label = "Input a JPEG image")
+    ),
+    # Main panel for displaying outputs ----
+    mainPanel(
+      # Output: Histogram ----
+      textOutput(outputId = "prediction"),
+      plotOutput(outputId = "image")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a histogram ----
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  image <- reactive({
+    req(input$image_path)
+    jpeg::readJPEG(input$image_path$datapath)
+  })
+  
+  
+  output$prediction <- renderText({
+    
+    img <- image() %>% 
+      array_reshape(., dim = c(1, dim(.), 1))
+    #predict classes is depreciated https://stackoverflow.com/questions/72486353/r-keras-beginner-question-predict-classes
+    #predict_classes(model, img) does not work.
+    paste0("The predicted class number is ", apply(predict(model, img), 1, which.max) -1)
+  })
+  
+  output$image <- renderPlot({
+    plot(as.raster(image()))
+  })
+  
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
--- sample_minst_keras.R
+++ sample_minst_keras.R
@@ -2,5 +2,47 @@ library(keras)
 library(tensorflow)
 mnist <- dataset_mnist()
 
+setwd("/Users/new/Documents/github_projects/RShiny_CNN_deploy")
 
 
+#loadind a simple MNIST number model
+mnist$train$x <- (mnist$train$x/255) %>% 
+  array_reshape(., dim = c(dim(.), 1))
+
+mnist$test$x <- (mnist$test$x/255) %>% 
+  array_reshape(., dim = c(dim(.), 1))
+
+
+#here we will define a simple CNN
+model <- keras_model_sequential() %>% 
+  layer_conv_2d(filters = 16, kernel_size = c(3,3), activation = "relu") %>% 
+  layer_max_pooling_2d(pool_size = c(2,2)) %>% 
+  layer_conv_2d(filters = 16, kernel_size = c(3,3), activation = "relu") %>% 
+  layer_max_pooling_2d(pool_size = c(2,2)) %>% 
+  layer_flatten() %>% 
+  layer_dense(units = 128, activation = "relu") %>% 
+  layer_dense(units = 10, activation = "softmax")
+
+model %>% 
+  compile(
+    loss = "sparse_categorical_crossentropy",
+    optimizer = "adam",
+    metrics = "accuracy"
+  )
+
+#Now we will fit our model
+model %>% 
+  fit(
+    x = mnist$train$x, y = mnist$train$y,
+    batch_size = 32,
+    epochs = 5,
+    validation_sample = 0.2,
+    verbose = 2
+  )
+
+#evaluating the performance
+model %>% evaluate(x = mnist$test$x, y = mnist$test$y)
+
+
+#Now we save our model
+save_model_tf(model, "Image_Classification_CAMH/cnn-mnist")
